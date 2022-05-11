b8882ace63935f3597475f0ad4a0c534551a73ab
Ethan Link <linkeh@mail.uc.edu>
2022-05-11 13:37:59 -0400

    Pickle start

--- XMEGA/custom_capture/analyze.py
+++ XMEGA/custom_capture/analyze.py
@@ -1,4 +1,4 @@
-#!python3
+#!/bin/python3
 import string
 import numpy as np
 from math import ceil
--- XMEGA/custom_capture/xmegarun.py
+++ XMEGA/custom_capture/xmegarun.py
@@ -8,7 +8,7 @@ target = cw.target(scope, cw.targets.SimpleSerial)
 print(target)
 print(scope.fw_version)
 
-#cw.program_target(scope, cw.programmers.XMEGAProgrammer, "/home/ethan/Documents/Git/chipwhisperer/hardware/victims/firmware/simpleserial-aes/simpleserial-aes-CW303.hex")
+#cw.program_target(scope, cw.programmers.XMEGAProgrammer, "/home/erc528/chipwhisperer/hardware/victims/firmware/simpleserial-aes/simpleserial-aes-CW303.hex")
 
 
 import time
@@ -21,6 +21,7 @@ time.sleep(0.1)
 scope.io.pdic = 'high_z' #XMEGA doesn't like pdic driven high
 time.sleep(0.1) #xmega needs more startup time
 
+pairs = []
 
 ktp = cw.ktp.Basic()
 key, msg = ktp.next()
@@ -30,6 +31,14 @@ scope.arm()
 print(":(")
 for i in range(50):
     target.simpleserial_write('p', msg)
+    resp = target.simpleserial_read('r', 16)
 
-    print(target.simpleserial_read('r', 16))
-    _, msg = ktp.next()
+    pairs.append([msg, resp, key])
+
+    key, msg = ktp.next()
+
+import pickle
+
+pickle.dump(pairs, open('pairs', 'ab'))
+
+
